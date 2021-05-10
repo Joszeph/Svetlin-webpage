5c043d41124b29a1cf846e303b05e8e6235531e8
Yosif Georgiev <iodesign@live.com>
2021-05-10 14:32:22 +0300

    add map

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,42 +1,24 @@
-import { useState } from "react"
-import ReactMapGL,{ Marker } from 'react-map-gl'
-
-
-const Map =({locations})=> {
-    const [viewport, setViewport] = useState({
-        width: '50vh',
-        height: '50vh',
-        latitude:  43.20269505233881,
-        longitude: 27.906136997158796,
-        zoom: 15
-      })
-    
-      return <ReactMapGL 
-      mapStyle="mapbox://styles/mapbox/streets-v11"
-      mapboxApiAccessToken="pk.eyJ1Ijoiam9zemVwaCIsImEiOiJja29laGJwdHQwY2E5Mm5yMHJiMWluMnVsIn0.nbrq8nNqcmgH8TQu4IEV8A"
-      {...viewport}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      >
-      {locations.map((location) => (
-        <div key={location.id}>
-          <Marker
-          latitude={location.center[1]}
-          longitude={location.center[0]}
-          offsetLeft={-20}
-          offsetTop={-10}>
-            <span role="img" aria-label="push-pin" >📌ул. „Александър Дякович” No 45, ет. 4</span>
-          </Marker>
-          <style jsx>{`
-          span{
-              color:red;
-              font-weight:bold;
-              font-size:0.8em;
-              padding-left:38px;
-          }
-          `}</style>
+const Map =()=> {
+      return(
+         <div className="map-responsive">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2908.327733574471!2d27.903925915483242!3d43.202614879139276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4538cbe578aa1%3A0x227314e4f4ebbdff!2sul.%20%22Aleksander%20Dyakovich%22%2045%2C%209000%20Varna%20Center%2C%20Varna!5e0!3m2!1sen!2sbg!4v1620642774680!5m2!1sen!2sbg" loading="lazy" width="380" height="520" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0" ></iframe>
+        <style jsx>{`
+          .map-responsive{
+            overflow:hidden;
+            padding:46%;
+            position:relative;
+            height:0;
+        }
+        .map-responsive iframe{
+            left:0;
+            top:0;
+            height:100%;
+            width:100%;
+            position:absolute;
+        }
+        `}</style>
         </div>
-      ))}
-    </ReactMapGL>
+      )
     }
 
 
--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,4 +1,3 @@
-import{useState,useEffect} from 'react'
 import ContactForm from "../../components/ContactForm"
 import styles from './contacts.module.css'
 
@@ -8,23 +7,8 @@ const Map = dynamic(() => import('../../components/Map'), {
     loading: () => "Loading...",
     ssr: false
   })
-
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/27.906136997158796%2C43.20269505233881.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1620414456346&autocomplete=true&types=address%2Ccountry`;
   
-const Contacts = () => {
-
-    const [locations, setLocations] = useState([]);
-  useEffect(() => {
-    const fetchLocations = async () => {
-      await fetch(url).then((response) =>
-        response.text()).then((res) => JSON.parse(res))
-      .then((json) => {
-        setLocations(json.features);
-      }).catch((err) => console.log({ err }));
-    };
-    fetchLocations();
-  }, []);
-    
+const Contacts = () => { 
     return ( 
         <main>
             <div className={styles.title}>
@@ -32,14 +16,12 @@ const Contacts = () => {
             </div>
             <div className={styles.container}>
             <section className={styles.left}>
-                  <h2>Свържете се с мен:</h2>
-                  <ContactForm />
-              </section>
-              <section className={styles.right}>
-              <div className={styles.mapContainer}>
-            <Map locations={locations}/>
-          </div>
-              </section>
+                <Map/>  
+            </section>
+            <section className={styles.right}>
+              <h2>Свържете се с мен:</h2>
+              <ContactForm />
+            </section>
           </div>
         </main>
     
