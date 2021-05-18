3d5b3451e93f0ae4dcd65421c146a378ec9a4312
Yosif Georgiev <iodesign@live.com>
2021-05-18 14:24:53 +0300

    fix

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,125 +1,130 @@
-// import { createClient } from 'contentful'
-// import{documentToReactComponents} from '@contentful/rich-text-react-renderer'
+import { createClient } from 'contentful'
+import{documentToReactComponents} from '@contentful/rich-text-react-renderer'
 import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
-// const client = createClient({
-//     space: process.env.CONTENTFUL_SPACE_ID,
-//     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-//   })
+// import fetch from 'isomorphic-unfetch'
+const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
 
-// export const getStaticPaths = async () => {
-//     const res = await client.getEntries({ 
-//       content_type: "recipe" 
-//     })
+export const getStaticPaths = async () => {
+    const res = await client.getEntries({ 
+      content_type: "recipe" 
+    })
   
-//     const paths = res.items.map(item => {
-//       return {
-//         params: { slug: item.fields.slug }
-//       }
-//     })
+    const paths = res.items.map(item => {
+      return {
+        params: { slug: item.fields.slug }
+      }
+    })
   
-//     return {
-//       paths,
-//       fallback: true
-//     }
-// }
-
-// export const getStaticProps = async ({ params }) => {
-//     const { items } = await client.getEntries({
-//       content_type: 'recipe',
-//       'fields.slug': params.slug
-//     })
-  
-//     if (!items.length) {
-//       return {
-//         redirect: {
-//           destination: '/error-404',
-//           permanent: false,
-//         },
-//       }
-//     }
-  
-//     return {
-//       props: { recipe: items[0] },
-//       revalidate: 1
-//     }
-  
-// }
-
-// const Article = ({recipe})=>{
-//     const{title,method} = recipe.fields
-//     return(
-//         <div className="container">
-//             <h2>{title}</h2>
-//             <div>{documentToReactComponents(method)}</div>
-//             <div className="link"><Link href="/blog"><a>Обратно</a></Link></div>
-//             <style jsx>{`
-//             .container{
-//               max-width:1200px;
-//               margin:0 auto;
-//               line-height: 22px;
-//               padding: 20px;
-//             }
-//             .container h2{
-//               margin-bottom:10px;
-//             }
-//             .link{
-//               margin-top:10px;
-//             }
-//             .link a{
-//               color:#f20f0f;
-//               text-decoration:none;
-//             }
-//             `}</style>
-//         </div>
-//     )
-// }
-
-// export default Article
-const Article = ({item})=>{
-
-  return(
-      <div className="container">
-          <h2>{item.title}</h2>
-          <div>
-          <p dangerouslySetInnerHTML={{__html: item.description}} />
-          </div>
-          <div className="link"><Link href="/blog"><a>Обратно</a></Link></div>
-          <style jsx>{`
-          .container{
-            max-width:1200px;
-            margin:0 auto;
-            line-height: 22px;
-            padding: 20px;
-          }
-          .container h2{
-            margin-bottom:10px;
-          }
-          .link{
-            margin-top:10px;
-          }
-          .link a{
-            color:#f20f0f;
-            text-decoration:none;
-          }
-          `}</style>
-      </div>
-  )
+    return {
+      paths,
+      fallback: true
+    }
 }
 
-export async function getServerSideProps(context) {
-  const { slug } = context.query
-
-  const res = await fetch(`https://strapi-next-movie-demo.herokuapp.com/movies?slug=${slug}`)
+export const getStaticProps = async ({ params }) => {
+    const { items } = await client.getEntries({
+      content_type: 'recipe',
+      'fields.slug': params.slug
+    })
   
-  const data = await res.json()
-  return {
-      props: {
-          item: data[0]
-      },
-  }
+    if (!items.length) {
+      return {
+        redirect: {
+          destination: '/error-404',
+          permanent: false,
+        },
+      }
+    }
+  
+    return {
+      props: { recipe: items[0] },
+      revalidate: 1
+    }
+  
+}
+
+const Article = ({recipe})=>{
+    const{title,method} = recipe.fields
+    return(
+        <div className="container">
+            <h2>{title}</h2>
+            <div>{documentToReactComponents(method)}</div>
+            <div className="link"><Link href="/blog"><a>Обратно</a></Link></div>
+            <style jsx>{`
+            .container{
+              max-width:1200px;
+              margin:0 auto;
+              line-height: 22px;
+              padding: 20px;
+            }
+            .container h2{
+              margin-bottom:10px;
+            }
+            .link{
+              margin-top:10px;
+            }
+            .link a{
+              color:#f20f0f;
+              text-decoration:none;
+            }
+            `}</style>
+        </div>
+    )
 }
 
 export default Article
+
+
+
+
+// !SSR
+// const Article = ({item})=>{
+
+//   return(
+//       <div className="container">
+//           <h2>{item.title}</h2>
+//           <div>
+//           <p dangerouslySetInnerHTML={{__html: item.description}} />
+//           </div>
+//           <div className="link"><Link href="/blog"><a>Обратно</a></Link></div>
+//           <style jsx>{`
+//           .container{
+//             max-width:1200px;
+//             margin:0 auto;
+//             line-height: 22px;
+//             padding: 20px;
+//           }
+//           .container h2{
+//             margin-bottom:10px;
+//           }
+//           .link{
+//             margin-top:10px;
+//           }
+//           .link a{
+//             color:#f20f0f;
+//             text-decoration:none;
+//           }
+//           `}</style>
+//       </div>
+//   )
+// }
+
+// export async function getServerSideProps(context) {
+//   const { slug } = context.query
+
+//   const res = await fetch(`https://strapi-next-movie-demo.herokuapp.com/movies?slug=${slug}`)
+  
+//   const data = await res.json()
+//   return {
+//       props: {
+//           item: data[0]
+//       },
+//   }
+// }
+
+// export default Article
 
 
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,70 +1,72 @@
  import Card from '../../components/Card.jsx'
+//  import fetch from 'isomorphic-unfetch'
  import styles from './blog.module.css'
 
- import fetch from 'isomorphic-unfetch'
 
-// import {createClient} from 'contentful'
+import {createClient} from 'contentful'
 
-// import "aos/dist/aos.css";
+import "aos/dist/aos.css";
 
-// export async function getStaticProps(){
+export async function getStaticProps(){
 
-//   const client = createClient({
-//     space: process.env.CONTENTFUL_SPACE_ID,
-//     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-//   })
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
 
-//   const res = await client.getEntries({content_type: 'recipe'})
-
-//   return{
-//     props: {recipes: res.items },
-//     revalidate: 1
-//   }
-// }
-
-// const Blog = ({recipes}) => {
-//   console.log(recipes)
-//     return ( 
-//         <main className={styles.container}>
-//           <div className={styles.title} data-aos="fade-right">
-//             <h1>Правен блог</h1>
-//           </div>
-//             <div> 
-//             {recipes.map(recipe =>(
-//           <Card recipe={recipe} key={recipe.sys.id}/>
-//       ))}
-//             </div>
-//         </main>
-//      );
-// }
- 
-// export default Blog;
-
-export async function getServerSideProps(){
-    
-
-  const res = await fetch(`https://strapi-next-movie-demo.herokuapp.com/movies`)
-  const data = await res.json()
+  const res = await client.getEntries({content_type: 'recipe'})
 
   return{
-    props:{
-      items:data
-    }
+    props: {recipes: res.items },
+    revalidate: 1
   }
 }
 
-
-const Blog = ({items}) => {
+const Blog = ({recipes}) => {
+  console.log(recipes)
     return ( 
         <main className={styles.container}>
           <div className={styles.title} data-aos="fade-right">
             <h1>Правен блог</h1>
           </div>
             <div> 
-          <Card items={items} />
+            {recipes.map(recipe =>(
+          <Card recipe={recipe} key={recipe.sys.id}/>
+      ))}
             </div>
         </main>
      );
 }
  
 export default Blog;
+
+
+//!SSR
+// export async function getServerSideProps(){
+    
+//   const res = await fetch(`https://strapi-next-movie-demo.herokuapp.com/movies`)
+//   const data = await res.json()
+
+//   return{
+//     props:{
+//       items:data
+//     }
+//   }
+// }
+
+
+
+// const Blog = ({items}) => {
+//     return ( 
+//         <main className={styles.container}>
+//           <div className={styles.title} data-aos="fade-right">
+//             <h1>Правен блог</h1>
+//           </div>
+//             <div>  
+//             <Card items={items}/>
+//             </div>
+//         </main>
+//      );
+// }
+ 
+// export default Blog;
