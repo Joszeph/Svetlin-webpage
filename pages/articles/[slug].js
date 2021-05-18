86be5346efa72d2de93abb73c165ebe21a9bbc62
Yosif Georgiev <iodesign@live.com>
2021-05-18 14:29:25 +0300

    fix

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -2,6 +2,7 @@ import { createClient } from 'contentful'
 import{documentToReactComponents} from '@contentful/rich-text-react-renderer'
 import Link from 'next/link'
 // import fetch from 'isomorphic-unfetch'
+
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
   
     return {
       paths,
-      fallback: true
+      fallback: false
     }
 }
 
