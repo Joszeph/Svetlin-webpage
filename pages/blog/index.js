9b5a1f7ba51aa6094e42f31cddffab94adbdc944
Yosif Georgiev <iodesign@live.com>
2021-05-20 18:22:45 +0300

    add sanity cms

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,71 +1,69 @@
- import Card from '../../components/Card.jsx'
-//  import fetch from 'isomorphic-unfetch'
- import styles from './blog.module.css'
+import styles from './blog.module.css'
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 
-import {createClient} from 'contentful'
+const Blog = ({posts}) => {
 
+  const router = useRouter();
+  const [mappedPosts, setMappedPosts] = useState([]);
 
-export async function getStaticProps(){
+  useEffect(() => {
+    if (posts.length) {
+      // const imgBuilder = imageUrlBuilder({
+      //   projectId: 'mjoyrhci',
+      //   dataset: 'production',
+      // });
 
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  })
+      setMappedPosts(
+        posts.map(p => {
+          return {
+            ...p,
+            // mainImage: imgBuilder.image(p.mainImage).width(500).height(250),
+          }
+        })
+      );
+    } else {
+      setMappedPosts([]);
+    }
+  }, [posts]);
 
-  const res = await client.getEntries({content_type: 'recipe'})
-
-  return{
-    props: {recipes: res.items },
-    revalidate: 1
-  }
-}
-
-const Blog = ({recipes}) => {
-  console.log(recipes)
     return ( 
         <main className={styles.container}>
           <div className={styles.title} data-aos="fade-right">
             <h1>Правен блог</h1>
           </div>
             <div> 
-            {recipes.map(recipe =>(
-          <Card recipe={recipe} key={recipe.sys.id}/>
-      ))}
+            {mappedPosts.length ? mappedPosts.map((p, index) => (
+            <div onClick={() => router.push(`/post/${p.slug.current}`)} key={index} className={styles.post}>
+              <h3>{p.title}</h3>
+              {/* <img className={styles.mainImage} src={p.mainImage} /> */}
+            </div>
+          )) : <>No Posts Yet</>}
             </div>
         </main>
      );
 }
+
+export const getServerSideProps = async pageContext => {
+  const query = encodeURIComponent('*[ _type == "post" ]');
+  const url = `https://kna6xtr1.api.sanity.io/v1/data/query/production?query=${query}`;
+  const result = await fetch(url).then(res => res.json());
+
+  if (!result.result || !result.result.length) {
+    return {
+      props: {
+        posts: [],
+      }
+    }
+  } else {
+    return {
+      props: {
+        posts: result.result,
+      }
+    }
+  }
+};
  
 export default Blog;
-
-
-//!SSR
-// export async function getServerSideProps(){
-    
-//   const res = await fetch(`https://strapi-next-movie-demo.herokuapp.com/movies`)
-//   const data = await res.json()
-
-//   return{
-//     props:{
-//       items:data
-//     }
-//   }
-// }
-
-
-
-// const Blog = ({items}) => {
-//     return ( 
-//         <main className={styles.container}>
-//           <div className={styles.title} data-aos="fade-right">
-//             <h1>Правен блог</h1>
-//           </div>
-//             <div>  
-//             <Card items={items}/>
-//             </div>
-//         </main>
-//      );
-// }
- 
-// export default Blog;
---
+++ pages/post/[slug].js
@@ -0,0 +1,69 @@
+import Link from 'next/link'
+import BlockContent from '@sanity/block-content-to-react';
+
+
+const Post = ({title, body})=>{
+    return(
+        <div className="container">
+            <h2>{title}</h2>
+            <div>
+            <BlockContent blocks={body} />
+            </div>
+            <div className="link"><Link href="/blog"><a>Обратно</a></Link></div>
+            <style jsx>{`
+            .container{
+              max-width:1200px;
+              margin:0 auto;
+              line-height: 22px;
+              padding: 20px;
+            }
+            .container h2{
+              margin-bottom:10px;
+            }
+            .link{
+              margin-top:10px;
+            }
+            .link a{
+              color:#f20f0f;
+              text-decoration:none;
+            }
+            `}</style>
+        </div>
+    )
+}
+
+export const getServerSideProps = async pageContext => {
+  const pageSlug = pageContext.query.slug;
+  
+  if (!pageSlug) {
+    return {
+      notFound: true
+    }
+  }
+
+  const query = encodeURIComponent(`*[ _type == "post" && slug.current == "${pageSlug}" ]`);
+  const url = `https://kna6xtr1.api.sanity.io/v1/data/query/production?query=${query}`;
+
+  const result = await fetch(url).then(res => res.json());
+  const post = result.result[0];
+
+  if (!post) {
+    return {
+      notFound: true
+    }
+  } else {
+    return {
+      props: {
+        body: post.body,
+        title: post.title,
+      }
+    }
+  }
+};
+
+export default Post
+
+
+
+
+
