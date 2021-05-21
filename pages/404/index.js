f08fbf2ee78b3d4fcd623aef41c2cf9c86ff209d
Yosif Georgiev <iodesign@live.com>
2021-05-21 21:33:00 +0300

    add Error Page 404

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,7 +1,24 @@
+import Link from 'next/link'
+
 const ErrorPage = () => {
     return ( 
-        <div>
+        <div className="container">
+            <h1>Страницата не е намерена. Грешка 404!</h1>
             <h1>Page not found. Error 404!</h1>
+            <Link href="/"><a>Обратно/Back...</a></Link>
+            <style jsx>{`
+                .container{
+                    max-width:1200px;
+                    margin:0 auto;
+                    color:#f20f0f;
+                    padding:250px;
+                    line-height:40px;
+                }
+                .container a{
+                    text-decoration:none;
+                    color:#f20f0f;
+                }
+            `}</style>
         </div>
      );
 }
