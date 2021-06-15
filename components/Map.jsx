4f5505994fcad3457876759ccefe01e5cefa922d
Yosif Georgiev <iodesign@live.com>
2021-06-15 20:21:54 +0300

    fix Map location

--- components/Map.jsx
+++ components/Map.jsx
@@ -5,7 +5,7 @@ const Map =()=> {
 
       return(
          <div className="map-responsive">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2908.327733574471!2d27.903925915483242!3d43.202614879139276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a4538cbe578aa1%3A0x227314e4f4ebbdff!2sul.%20%22Aleksander%20Dyakovich%22%2045%2C%209000%20Varna%20Center%2C%20Varna!5e0!3m2!1sen!2sbg!4v1620642774680!5m2!1sen!2sbg" loading="lazy" width="100%" height="520" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0" ></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d727.0782590309101!2d27.90594311779098!3d43.20292318788159!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x58deec9cec604902!2z0LDQtNCy0L7QutCw0YIg0KHQstC10YLQu9C40L0g0KLQvtC00L7RgNC-0LI!5e0!3m2!1sen!2sbg!4v1623776478552!5m2!1sen!2sbg" loading="lazy" width="100%" height="520" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         <style jsx>{`
           box-shadow:4px 4px 10px rgba(0,0,0,0.06);
         `}</style>
