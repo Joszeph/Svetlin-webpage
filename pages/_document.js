e55d86d710456b2895dbd0aa85a6bffe0e117bd5
Yosif Georgiev <iodesign@live.com>
2021-06-21 15:48:29 +0300

    seo fix

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,11 @@ class MyDocument extends Document {
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="preload"/>
                 <meta name="theme-color" content="#33333"/>
-                {/* <meta property="og:image" content="https://www.todorovlaw.com/_next/image?url=%2Flogo.webp&w=256&q=75"/> */}
+                <meta property="og:image" content="https://www.todorovlaw.com/_next/image?url=%2Flogo.webp&w=256&q=75"/>
                 <meta property="og:image:type" content="image/png"/>
                 <meta property="og:image:width" content="228"/>
                 <meta property="og:image:height" content="122"/>
-                <meta name="description" content="Адвокат Светлин Тодоров - Адвокатска Кантора град Варна. Изготвяне на договори, правни становища, нотариални покани, искови молби, жалби срещу наказателни постановления и административни актове и др."/>
+                <meta name="description" content="Адвокат Светлин Тодоров - Адвокатска Кантора град Варна. Изготвяне на договори, правни становища, нотариални покани, жалби и административни актове и др."/>
                 <meta property="og:title" content="Адвокат Светлин Тодоров - град Варна. Изготвяне на договори, правни становища, нотариални покани, искови молби, жалби срещу наказателни постановления и административни актове и др. Адвокатска Кантора град Варна." />
                 <meta property="og:url" content="www.todorovlaw.com" />
                 <meta property="og:type" content="website" />
